ca345f5df57477af47002abe9caec234c78abfbc
000itsuki000 <itsuki.h@collabo-style.co.jp>
2023-12-18 20:47:58 +0900

    Initial Commit

---
+++ horizontal-scrollable-tabs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  horizontal-scrollable-tabs
+//
+//  Created by Itsuki on 2023/12/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
